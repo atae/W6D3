858302be794e75e332fe75bdabc64382aba91390
App Academy Student <student@appacademy.io>
2016-10-19 13:46:52 -0700

    Add disabled button feature

--- skeleton/app/assets/javascripts/bundle.js
+++ skeleton/app/assets/javascripts/bundle.js
@@ -69,22 +69,37 @@
 	  render(){
 	    if(this.followState === "followed") {
 	      this.$el.text("Unfollow!");
-	    } else {
+	      this.$el.attr("disabled", false);
+	    } else if (this.followState === "unfollowed") {
 	      this.$el.text("Follow!");
+	      this.$el.attr("disabled", false);
+	    } else if (this.followState === "following") {
+	      this.$el.text("Following...");
+	      this.$el.attr("disabled", true);
+	    } else {
+	      this.$el.text("Unfollowing...");
+	      this.$el.attr("disabled", true);
 	    }
 	  }
 	
 	  setPendingState() {
 	    // fix following / followed stuffs
+	    if (this.followState === "followed") {
+	      this.followState = 'unfollowing';
+	    } else {
+	      this.followState = 'following';
+	    }
 	  }
 	
 	  handleClick(){
 	    // debugger
 	    this.$el.on("click", (e) => {
 	      this.setPendingState();
+	      this.render();
+	      // debugger
 	      e.preventDefault();
-	      let oppositeFollowstate = (this.followState === "followed" ? "unfollowed" : "followed");
-	      let requestType = (this.followState === "followed" ? "DELETE" : "POST");
+	      let oppositeFollowstate = (this.followState === "following" ? "followed" : "unfollowed");
+	      let requestType = (this.followState === "following" ? "POST" : "DELETE");
 	        $.ajax({
 	          url: `/users/${this.userId}/follow`,
 	          type: `${requestType}`,
